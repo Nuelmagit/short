c557d3d01a9b39e7db9e0450f0e0a5f86b137859
manuel <manuel@DESKTOP-3LFEVQQ.localdomain>
2022-10-03 22:30:03 -0300

    add

--- 14.js
+++ 14.js
@@ -1,3 +1,9 @@
 some js challengues 
 
 https://intspirit.medium.com/only-10-of-developers-can-solve-these-javascript-challenges-f4804c7c54eb
+
+
+
+Composicion versus await.
+
+como manejar errores y promsas
